b11b8d119b957cf70737528d744042939e5704ee
HOJO3 <joe.ho@oocl.com>
2018-11-20 17:44:56 +0800

    file update

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
 
-
+var multiplicationTable = "";
 function multiplyOverRangeOfTwoIntegers (start, end) {
-	if (start>end){
-		return null;
+	let maxLength = createEachFieldValue(start,end).length;
+	if(!isInputValid(start,end)){
+		return null
 	}
-	let multiplicationTable = "";
-	let tmp_text = "";
 	for (i = start;i<=end;i++){
-		for (j = start; j<=i;j++){
-			tmp_text += j.toString()+"*"+i.toString()+"="+(i*j).toString();
-			if (j!=i){
-				tmp_text+="  ";
-			}
+		let tmp_text = AddSpace(start,i,maxLength);
+		multiplicationTable+=tmp_text;
+		if(i!=end){
+			multiplicationTable+="\n";
 		}
-		if (i!=end){
-			multiplicationTable+=tmp_text+"\n";
-		}else{
-			multiplicationTable+=tmp_text;
-		}
-		tmp_text = "";
 		
 	}
-
-    return multiplicationTable;
+	return multiplicationTable
 }
 
+function isInputValid(start,end){
+	return (start <= end && end<=1000)
+}
+
+function createEachFieldValue(i,j){
+	return i.toString()+"*"+j.toString()+"="+(i*j).toString();
+}
+
+function AddSpace(rowStart,rowEnd,maxLength){
+	let rowText = "";
+	for (k = rowStart;k<=rowEnd;k++){
+		let Fieldtext = createEachFieldValue(k,rowEnd);
+		Fieldtext += " ".repeat((maxLength - Fieldtext.length)+2);
+		rowText += Fieldtext;
+		}
+	return rowText
+}
+
+
+
 module.exports = multiplyOverRangeOfTwoIntegers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
