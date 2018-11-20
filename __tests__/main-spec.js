e3e63b73a4559f5bc1e14817288e6cd9a7781b92
HOJO3 <joe.ho@oocl.com>
2018-11-20 15:45:51 +0800

    js code completed

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,5 @@
-const add = require('../main');
-
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+const multiplyOverRangeOfTwoIntegers = require('../main');
+const text = '2*2=4\n' + '2*3=6  3*3=9\n' + '2*4=8  3*4=12  4*4=16';
+it ('should produce a mulitplication table', () => {
+    expect(multiplyOverRangeOfTwoIntegers(2, 4)).toBe(text);
 });
--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-function add (left, right) {
-    return left + right;
+function multiplyOverRangeOfTwoIntegers (start, end) {
+	if (start>end){
+		return null;
+	}
+	let text = "";
+	let tmp_text = "";
+	for (i = start;i<=end;i++){
+		for (j = start; j<=i;j++){
+			tmp_text += j.toString()+"*"+i.toString()+"="+(i*j).toString();
+			if (j!=i){
+				tmp_text+="  ";
+			}
+		}
+		if (i!=end){
+			text+=tmp_text+"\n";
+		}else{
+			text+=tmp_text;
+		}
+		tmp_text = "";
+		
+	}
+	//console.log(text);
+    return text;
 }
 
-module.exports = add;
+module.exports = multiplyOverRangeOfTwoIntegers;
