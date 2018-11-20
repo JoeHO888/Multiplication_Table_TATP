1f64c812ea96e635fe060bb0c1fc9058bd247127
HOJO3 <joe.ho@oocl.com>
2018-11-20 15:53:25 +0800

    js code updated

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,12 @@
 const multiplyOverRangeOfTwoIntegers = require('../main');
-const text = '2*2=4\n' + '2*3=6  3*3=9\n' + '2*4=8  3*4=12  4*4=16';
+
 it ('should produce a mulitplication table', () => {
+	//given integers 2,4
+	//when we call the function multiplyOverRangeOfTwoIntegers
+	const text = '2*2=4\n' + '2*3=6  3*3=9\n' + '2*4=8  3*4=12  4*4=16';
+	//then it should return a table  as below:
+	//2*2=4
+	//2*3=6 3*3=9
+	//2*4=8 3*4=12 4*4=16
     expect(multiplyOverRangeOfTwoIntegers(2, 4)).toBe(text);
 });
